0c55dc219f7e9a52278168f78dfd4920f2eab160
Álvaro Fuentes Lozano <167740606+afuentesl05@users.noreply.github.com>
2024-04-22 16:47:37 +0200

    Update AYC_Programacion_Dinamica_Tema4.py

--- AYC_Programacion_Dinamica_Tema4.py
+++ AYC_Programacion_Dinamica_Tema4.py
@@ -1,4 +1,5 @@
 # EJERCICIO 5
+import math
 
 
 def warshall(M):
@@ -22,21 +23,56 @@ def warshall(M):
     return C
 
 # Ejemplo de uso
+#if __name__ == "__main__":
+#    # Ejemplo de matriz de adyacencia M
+#    M = [
+#        [False, True, False, False],
+#        [False, False, True, True],
+#        [False, False, False, False],
+#        [True, False, True, False]
+#    ]
+#
+#    C = warshall(M)
+#    print("Matriz de caminos:")
+#    for row in C:
+#        print(row)
+
+
+#EJERCICIO 6
+
+
+def calcular_probabilidades_vc(vc):
+    total_vc = sum(vc)
+    probabilidades = [valor / total_vc for valor in vc]
+    return probabilidades
+
+def calcular_probabilidad_ganar_1_partido(probabilidades):
+    probabilidades_ganar = []
+    for i, prob in enumerate(probabilidades):
+        probabilidad_ganar = prob
+        for j, otra_prob in enumerate(probabilidades):
+            if i != j:
+                probabilidad_ganar *= (1 - otra_prob)
+        probabilidades_ganar.append(probabilidad_ganar)
+    return probabilidades_ganar
+
+def calcular_ganancias(vc, N, D):
+    probabilidades = calcular_probabilidades_vc(vc)
+    probabilidades_ganar = calcular_probabilidad_ganar_1_partido(probabilidades)
+    
+    probabilidad_grifos_ganar = math.pow(probabilidades_ganar[0], N) #en la lista en la posicion 0 esta la probabilidad de ganar de los grifos 1 partido
+
+    ganancias_maximas = D / probabilidad_grifos_ganar
+
+    return ganancias_maximas
+
+# Ejemplo de uso
 if __name__ == "__main__":
-    # Ejemplo de matriz de adyacencia M
-    M = [
-        [False, True, False, False],
-        [False, False, True, True],
-        [False, False, False, False],
-        [True, False, True, False]
-    ]
+    vc = [5, 4, 3, 2]  # Valores de Calidad de los equipos (Grifos, Serpientes, Cuervos, Tejones)
+    N = 3  # Cantidad de partidos que debe ganar un equipo para ganar el torneo
+    D = 100  # Cantidad de dinero apostado por Javi Potter
 
-    C = warshall(M)
-    print("Matriz de caminos:")
-    for row in C:
-        print(row)
-
-
-
+    ganancias = calcular_ganancias(vc, N, D)
+    print("Posibles ganancias si ganan los Grifos:", ganancias)
 
 
